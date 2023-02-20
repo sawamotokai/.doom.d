baa1d5cf68f40068526cf5baa4c7dd27e56e0f05
sawamotokai <kaisawamoto@gmail.com>
2023-02-19 20:16:05 -0800

    config

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
+ '(org-agenda-files
+   '("/home/kai/Documents/org/ubc/2022w2.org" "/home/kai/Documents/org/kai.org" "/home/kai/Documents/org/notes.org"))
+ '(package-selected-packages
+   '(org-bullets org-roam-ui evil-multiedit docker-tramp image-dired+ org-pomodoro dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company lsp-ui company hover)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-variable 'disabled nil)
